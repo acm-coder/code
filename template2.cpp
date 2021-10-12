d6c6f43f535f73f0615ab286ed7c65af18ed4e93
acm-code <1092163277@qq.com>
2021-10-12 23:15:27 +0800

    add template2.cpp

---
+++ template2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <cctype>
+namespace FastIO
+{
+char buf[1 << 21], buf2[1 << 21], a[20], *p1 = buf, *p2 = buf, hh1 = '\n', hh2 = ' ';
+int p, p3 = -1;
+void read() {}
+void print() { buf2[p3] = hh1; }
+inline int getc()
+{
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1 << 21, stdin), p1 == p2) ? EOF : *p1++;
+}
+inline void flush()
+{
+    fwrite(buf2, 1, p3 + 1, stdout), p3 = -1;
+}
+template <typename T, typename... T2>
+inline void read(T &x, T2 &... oth)
+{
+    int f = 0;
+    x = 0;
+    char ch = getc();
+    while (!isdigit(ch))
+    {
+        if (ch == '-')
+            f = 1;
+        ch = getc();
+    }
+    while (isdigit(ch))
+    {
+        x = x * 10 + ch - 48;
+        ch = getc();
+    }
+    x = f ? -x : x;
+    read(oth...);
+}
+template <typename T, typename... T2>
+inline void print(T x, T2... oth)
+{
+    if (p3 > 1 << 20)
+        flush();
+    if (x < 0)
+        buf2[++p3] = 45, x = -x;
+    do
+    {
+        a[++p] = x % 10 + 48;
+    } while (x /= 10);
+    do
+    {
+        buf2[++p3] = a[p];
+    } while (--p);
+    buf2[++p3] = hh2;
+    print(oth...);
+}
+} // namespace FastIO
+using FastIO::print;
+using FastIO::read;
+//======================================
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+#endif
+    clock_t c1 = clock();
+    //======================================
+    
+    //======================================
+    FastIO::flush();
+    std::cerr << "Time:" << clock() - c1 << "ms" << std::endl;
+    return 0;
+}
+/*
+ * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
+ * │Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│  ┌┐    ┌┐    ┌┐
+ * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘  └┘    └┘    └┘
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
+ * │~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│ │Num│ / │ * │ - │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
+ * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│ │ 7 │ 8 │ 9 │   │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┤ + │
+ * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │               │ 4 │ 5 │ 6 │   │
+ * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
+ * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │     │ 1 │ 2 │ 3 │   │
+ * ├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤ E││
+ * │ Ctrl│ Win│ Alt│         Space         │ Alt│ Win│Menu│Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │←─┘│
+ * └─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
+ */
