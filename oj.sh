ac1e3073b852914698f892402032e5ad0a7c3a7b
acm-code <1092163277@qq.com>
2022-05-01 11:34:18 +0800

    oj 添加模板代码重置

--- oj.sh
+++ oj.sh
@@ -1,2 +1,20 @@
-./compile.sh
-./diff.sh
+function do_case() {
+    echo $1
+    case $1 in
+        st_code)
+            git checkout -- code.cpp
+            ;;
+        st_all)
+            git checkout .
+            ;;
+        *)
+            ./compile.sh
+            ./diff.sh
+            ;;
+    esac
+}
+
+for arg in "$@"
+do
+    do_case $arg
+done
