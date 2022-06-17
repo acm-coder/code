f8019d3f0ec2ae8a0fb8f4c33f5dc8cf2d0f7840
coder-coding-the-code <1092163277@qq.com>
2022-06-17 20:18:31 +0800

    修改随机数生成器

--- data_generate.cpp
+++ data_generate.cpp
@@ -11,14 +11,16 @@ void generate_data() {
     std::default_random_engine dre(rd());
     std::uniform_int_distribution<int> uid(1, 1000);
     int cnt = uid(dre);
+    std::uniform_int_distribution<int> gid(1, cnt);
     cout << cnt << endl;
     for (int i = 1; i <= cnt; ++ i) {
-        cout << uid(dre) << ' ' << uid(dre) << endl;
+        cout << gid(dre) << ' ' << gid(dre) << endl;
     }
 }
 
 int main() {
     cin >> n;
+    cout << n << endl;
     while (n --) generate_data();
     return 0;
 }
