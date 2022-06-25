45ed8fefb652c1d2ead2054855de1d52ec35ac41
coder-coding-the-code <1092163277@qq.com>
2022-06-25 10:57:10 +0800

    添加compile make执行状态判定

--- compile.sh
+++ compile.sh
@@ -1,16 +1,26 @@
+function run() {
+    if [ $1 -eq 0 ]; then
+        $2 < $3 > $4
+    fi
+}
+
+
 if [ $# -eq 0 ]
 then
-    make && ./a.out < in > result
+    make
+    run $? ./a.out in result
 else
     case $1 in
         grd)
-            make $1 && ./grd < gen_in > in
+            make
+            run $? ./grd gen_in in
             ;;
         std)
             for i in {0..11}
             do
                 echo "./data_test/in_${i}"
-                make && ./a.out < "./data_test/in_${i}" > "./data_test/out_${i}"
+                make
+                run $? ./a.out  "./data_test/in_${i}" "./data_test/out_${i}"
             done
             ;;
     esac
